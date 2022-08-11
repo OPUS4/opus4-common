6f1c80d0844fabde926149ba7f8fd7ab000b58c6
j3nsch <j3nsch@gmail.com>
2022-08-11 13:24:40 +0200

    #59 Extend ModelInterface

--- src/DocumentInterface.php
+++ src/DocumentInterface.php
@@ -25,11 +25,13 @@
  * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * @copyright   Copyright (c) 2018-2022, OPUS 4 development team
+ * @copyright   Copyright (c) 2018, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  */
 
 namespace Opus\Common;
+
+use Opus\Common\Model\ModelInterface;
 
 /**
  * Interface for Document model objects.
@@ -37,7 +39,7 @@ namespace Opus\Common;
  * TODO add missing functions
  * TODO add documentation
  */
-interface DocumentInterface
+interface DocumentInterface extends ModelInterface
 {
     public function getId();
 
